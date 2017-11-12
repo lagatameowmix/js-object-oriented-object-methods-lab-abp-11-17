e32ffd7487897971389f6335b991f4a5f5f74736
Sasha V <sashav4@gmail.com>
2017-11-12 20:27:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function BoardMember(name, homeState, training){
     return("You will see great things from Scuber.")
   }
   this.sayHi = function(){
-    console.log("Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.")
+    return("Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.")
   }
 }
