eb2a995578ec0895c997fe9053e815f7a0545b21
Sasha V <sashav4@gmail.com>
2017-11-12 20:09:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ function BoardMember(name, homeState, training){
   this.name = name
   this.homeState = homeState
   this.training = training
-  this.veto = function() {
+  BoardMember.prototype.veto = function() {
     console.log('No, I must disagree')
   }
 }
-
-
