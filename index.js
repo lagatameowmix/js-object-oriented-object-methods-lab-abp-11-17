3fc90e474ad3800c103deef8665e9f0748528328
Sasha V <sashav4@gmail.com>
2017-11-12 20:19:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,7 @@ function BoardMember(name, homeState, training){
   this.releasePressStatement = function(){
     console.log("You will see great things from Scuber.")
   }
+  this.sayHi = function(){
+    console.log("Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.")
+  }
 }
